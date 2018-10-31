d0ebe3b107d5218c544bc6aaddf608de5d2a6e25
Magdalena Jankowska <magda@moldrzyk.pl>
2018-10-31 09:23:54 -0700

    Add search input field focus

--- src/GoogleMapMarkers.js
+++ src/GoogleMapMarkers.js
@@ -3,11 +3,22 @@ import PropTypes from 'prop-types';
 import GoogleMapListMarkers from './GoogleMapListMarkers'
 
 class GoogleMapMarkers extends Component {
+  constructor(props) {
+    super(props);
+    this.focusSearch = () => {
+      this.search.focus();
+    };
+  }
+
   static propTypes = {
     markers: PropTypes.array.isRequired,
     filterPlaces: PropTypes.func.isRequired,
     onMarkerClick: PropTypes.func.isRequired
   };
+
+  componentDidMount() {
+    this.focusSearch();
+  }
 
   // Populate query and kick off search
   onInputChange = () => {
@@ -22,6 +33,7 @@ class GoogleMapMarkers extends Component {
         <h2 className="markersTitle">Visible markers</h2>
         <form>
           <input
+            type="text"
             placeholder="Search for place..."
             tabIndex="1"
             ref={input => this.search = input}
